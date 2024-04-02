9065f8a481f59c3b6a9588cb95d4b07cc070a7a7
Rodrigo Barro <rodrigo.correa.barro@gmail.com>
2024-04-02 17:54:49 -0300

    add fastify and jest

--- apps/customers/src/index.ts
+++ apps/customers/src/index.ts
@@ -1,18 +1,18 @@
-import Fastify from 'fastify'
+import Fastify from "fastify";
 
-const fastify = Fastify()
+const fastify = Fastify();
 
-fastify.get('/', async(request, reply) => {
-    return 'Hello world';
+fastify.get("/", async (request, reply) => {
+  return "Hello world";
 });
 
 const start = async () => {
-    try {
-        await fastify.listen({ port: 8000 });
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
-    }
-}
+  try {
+    await fastify.listen({ port: 8000 });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
 
 start();
